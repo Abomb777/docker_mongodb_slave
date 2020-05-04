a5c1d47625af788c7443b6162646589146e2ddc6
Abomb777 <abomb82@gmail.com>
2020-05-05 02:01:55 +0300

    file name update13

--- config/deployment_scripts.sh
+++ config/deployment_scripts.sh
@@ -2,4 +2,4 @@
 
 cat /opt/mongo/hosts >> /etc/hosts
 chmod 400 /opt/mongo/mongo-keyfile
-/usr/bin/mongod --bind_ip_all --keyFile /opt/mongo/mongo-keyfile --replSet rs0
+/usr/bin/mongod --auth --bind_ip_all --keyFile /opt/mongo/mongo-keyfile --replSet rs0
